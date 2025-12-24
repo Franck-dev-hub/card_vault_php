8eceec5d55a892e980eb674ea535540ad217163e
Franck S. <franck.spadotto@yahoo.com>
2025-12-24 18:02:46 +0100

    feat(profile): add warning if card languge other than english is selected

---
+++ public/js/cardLanguagePopUp.js
@@ -0,0 +1,16 @@
+function cardLanguagePopUp(select) {
+    const selectedLang = select.value;
+    const message = select.dataset.warningMessage;
+
+    if (selectedLang !== "en") {
+        const confirmed = confirm(message);
+
+        if (confirmed) {
+            select.form.submit();
+        } else {
+            select.value = "en";
+        }
+    } else {
+        select.form.submit();
+    }
+}
