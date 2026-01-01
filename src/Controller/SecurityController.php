eb01fe5d8707a0b0538081fa3b40c0e0378f333d
Franck S. <franck.spadotto@yahoo.com>
2026-01-01 18:30:51 +0100

    fix(login): remember me cookie

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,7 @@
 namespace App\Controller;
 
 use App\Form\LoginFormType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
